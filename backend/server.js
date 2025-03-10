f9b4cf0bbb5b29bce87ffea153395c5c929ba43b
Ishita Jain <ishitajain385@gmail.com>
2025-03-10 17:42:52 +0530

    created .env file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
 const express= require('express')
-
+require('dotenv').config();
 const app= express()
+
+app.get('/',(req,res)=>{
+res.send("welcom to ai")
+})
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{console.log(`Server started on port ${port}`)})
